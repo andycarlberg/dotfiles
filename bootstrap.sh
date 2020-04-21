b10e31010908214f3b63eed67f432c37c1d29544
Andy Carlberg <andycarlberg@gmail.com>
2020-04-21 15:47:44 -0500

    Find the correct path to bootstrap.sh
    Note: This is only going to work some of the time. Going after the 80%, not the 20% with this change. See https://stackoverflow.com/a/29835459

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,9 @@
 # machines.
 
 # Defaults
-export DOTFILES=${DOTFILES:-"$(dirname "$0")"}
+# Note: this will only be correct *most* of the time. It's covering the 80%, 
+# not the 20%. See https://stackoverflow.com/a/29835459
+export DOTFILES=${DOTFILES:-"$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)"}
 
 # Find modules and run each install script.
 for module in $( ls -A ${DOTFILES}/modules) ; do
