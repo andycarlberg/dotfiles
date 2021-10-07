c069462415f1771be65ff969a2dd1cd66b83880e
Andy Carlberg <andy.carlberg@bounteous.com>
2021-10-07 14:02:27 -0500

    Begin re-configuring neovim.

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,26 @@
+-- Bootstrap packer
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd 'packadd packer.nvim'
+end
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Completion and linting
+  use {
+    'neovim/nvim-lspconfig',
+  }
+
+  -- Highlights
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    requires = {
+      'nvim-treesitter/nvim-treesitter-refactor',
+    },
+    run = ':TSUpdate',
+  }
+end)
