42ed88eb740245fc88e612563ecb9c2cf87ca6bb
Andy Carlberg <andy.carlberg@bounteous.com>
2022-09-22 10:58:22 -0500

    Enable debugger for lvim

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -63,21 +63,20 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
+lvim.builtin.dap.active = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
-  "c",
   "javascript",
   "json",
   "lua",
+  "php",
   "python",
   "typescript",
   "tsx",
   "css",
-  "rust",
-  "java",
   "yaml",
 }
 
