7199b9ef69eab001b39b1efb254ffedd7da6d84c
Andy Carlberg <andy.carlberg@bounteous.com>
2023-09-15 09:55:09 -0500

    Switch to antidote.
    zinit is too obtuse.

---
+++ .zsh/antidote_plugins.zsh
@@ -0,0 +1,3 @@
+fpath+=( /Users/andycarlberg/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )
+source /Users/andycarlberg/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel10k.zsh-theme
+source /Users/andycarlberg/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel9k.zsh-theme
