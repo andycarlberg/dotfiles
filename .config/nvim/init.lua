fdeebbc08eb1471d98af01609c33f0f9c720c605
Andy Carlberg <andycarlberg@gmail.com>
2023-09-17 09:25:19 -0500

    Reorganize misc basic plugins.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -67,13 +67,6 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
-  -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
-
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
-
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
--- .config/nvim/lua/custom/plugins/editor.lua
+++ .config/nvim/lua/custom/plugins/editor.lua
@@ -3,16 +3,22 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+      { "<leader>ft", "<cmd>NvimTreeToggle<cr>", desc = "Toggle filetree" },
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
   },
-  keys = {
-    { "<leader>ft", "<cmd>NvimTreeToggle<cr>", desc = "Toggle filetree" },
+  { 
+    -- Heuristically set expandtab and shiftwidth
+    "tpope/vim-sleuth" 
   },
-  config = function()
-    require("nvim-tree").setup {}
-  end,
 }
