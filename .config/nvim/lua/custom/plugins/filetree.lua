ef39196132175a9bb2999357227503ef60d81667
Andy Carlberg <andycarlberg@gmail.com>
2023-09-16 20:19:55 -0500

    Add kickstart.nvim config with nvim-tree.

---
+++ .config/nvim/lua/custom/plugins/filetree.lua
@@ -0,0 +1,18 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  keys = {
+    { "<leader>ft", "<cmd>NvimTreeToggle<cr>", desc = "Toggle filetree" },
+  },
+  config = function()
+    require("nvim-tree").setup {}
+  end,
+}
