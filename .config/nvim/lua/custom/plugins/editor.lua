68403ee4e9d4c7aad85999e3d9dc6ad6a3033d43
Andy Carlberg <andy.carlberg@bounteous.com>
2023-11-17 16:21:27 -0600

    Update telescope.nvim config.

--- .config/nvim/lua/custom/plugins/editor.lua
+++ .config/nvim/lua/custom/plugins/editor.lua
@@ -3,29 +3,61 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 return {
+
+  -- Fuzzy Finder (files, lsp, etc)
   {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
     dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    keys = {
-      { "<leader>ft", "<cmd>NvimTreeToggle<cr>", desc = "Toggle filetree" },
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope-file-browser.nvim',
+      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+      -- Only load if `make` is available. Make sure you have the system
+      -- requirements installed.
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        -- NOTE: If you are having trouble with this installation,
+        --       refer to the README for telescope-fzf-native for more instructions.
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
     },
     config = function()
-      require("nvim-tree").setup {}
-    end,
+      require('telescope').setup({
+        extensions = {
+          file_browser = {
+            -- disables netrw and use telescope-file-browser in its place
+            hijack_netrw = true,
+          }
+        }
+      })
+
+      -- Keymappings
+      vim.api.nvim_set_keymap(
+        'n',
+        '<space>fb',
+        ':Telescope file_browser path=%:p:h select_buffer=true<CR>',
+        {
+          noremap = true,
+          desc = 'Telescope file browser'
+        }
+      )
+
+      require('telescope').load_extension 'file_browser'
+      require('telescope').load_extension 'fzf'
+    end
   },
 
   {
     -- Heuristically set expandtab and shiftwidth
-    "tpope/vim-sleuth"
+    'tpope/vim-sleuth'
   },
 
   {
     'windwp/nvim-autopairs',
-    event = "InsertEnter",
+    event = 'InsertEnter',
     opts = {} -- this is equalent to setup({}) function
   }
 }
