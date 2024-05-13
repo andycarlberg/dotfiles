5edd7904c98869439b10333948f610873cbe3849
Andy Carlberg <andy.carlberg@bounteous.com>
2024-05-13 14:36:32 -0500

    Update nvim keymaps

--- .config/nvim/lua/custom/plugins/ui.lua
+++ .config/nvim/lua/custom/plugins/ui.lua
@@ -1,9 +1,9 @@
 return {
-  { 
+  {
     "alexghergh/nvim-tmux-navigation",
     keys = {
       { "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>" },
-      { "<C-j>", "<cmd>NvimTmuxNavigateDowm<cr>" },
+      { "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>" },
       { "<C-k>", "<cmd>NvimTmuxNavigateUp<cr>" },
       { "<C-l>", "<cmd>NvimTmuxNavigateRight<cr>" },
     },
