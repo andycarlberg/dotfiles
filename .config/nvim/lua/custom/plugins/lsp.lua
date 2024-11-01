f7162e576e59c9afb9f256ca31b24523923fa93d
Andy Carlberg <andycarlberg@gmail.com>
2024-11-01 09:26:38 -0500

    Update nvim keymaps, include lando path

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -37,7 +37,7 @@ return {
         end
 
         nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-        nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+        nmap('<leader>ca', vim.lsp.buf.code_action, 'Action')
 
         nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
         nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
