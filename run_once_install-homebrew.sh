a1f377b865e74ba151485f6b6cf824bb3d743313
Andy Carlberg <andycarlberg@users.noreply.github.com>
2025-11-13 08:44:46 -0600

    Add support for Homebrew installation on Apple Silicon

--- run_once_install-homebrew.sh
+++ run_once_install-homebrew.sh
@@ -5,3 +5,4 @@
 # enable homebrew for the current session
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+test -d /opt/homebrew/ && eval "$(/opt/homebrew/bin/brew shellenv)"
